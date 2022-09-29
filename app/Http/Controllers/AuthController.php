af40e767b3940e45420896f3b9b5b5131d38ba13
fuad <fuad.pcd@gmail.com>
2022-09-29 10:25:28 +0700

    create alur register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        # code...
+        $data = $request->all();
+        $data['password'] = Hash::make($request->password);
+        User::create($data);
+
+        return response()->json(
+            [
+                "response_code" => 2009900,
+                "response_message" => "Successful"
+            ]
+        );
     }
 }
