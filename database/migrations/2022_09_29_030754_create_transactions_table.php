edbfe31959c700800498725c99a1bcc604d5715c
fuad <fuad.pcd@gmail.com>
2022-09-29 10:13:07 +0700

    create migration transaction and model transaction

--- database/migrations/2022_09_29_030754_create_transactions_table.php
+++ database/migrations/2022_09_29_030754_create_transactions_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 100);
+            $table->string('status', 10)->nullable();
+            $table->char('reference_number', 20)->nullable();
+            $table->smallInteger('quantity');
             $table->integer('price');
-            $table->smallInteger("stock");
+            $table->integer('total_price');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('product_id')->constrained('products');
             $table->timestamps();
         });
     }
@@ -29,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('transactions');
     }
 };
